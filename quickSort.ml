999516731e8d1fc491467f2e7c7dcf35700541f6
Thomas <77362446+Thomas141203@users.noreply.github.com>
2024-02-28 15:43:04 +0100

    add quicksort

--- quickSort.ml
+++ quickSort.ml
@@ -1,8 +1,7 @@
 let rec quicksort = function
-  | [] -> []
-  | pivot :: rest ->
-      let lesser, greater = List.partition (fun x -> x < pivot) rest in
-      quicksort lesser @ [pivot] @ quicksort greater
+    | [] -> []
+    | pivot :: rest ->
+        let lesser, greater = List.partition (fun x -> x < pivot) rest in quicksort lesser @ [pivot] @ quicksort greater
 
 let nums = [3; 1; 4; 1; 5; 9; 2; 6; 5; 3; 5]
 let sorted_nums = quicksort nums
